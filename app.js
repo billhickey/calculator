98cfc77bb994c51504336b8d55ffb373dbbc8cfa
Bill Hickey <bhickey77@gmail.com>
2020-07-30 18:10:57 -0500

    implement live common calculator

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 let last10Calculations = [];
+let equation = [0];
+let previousEquation = [];
+let previousResult = 0;
+let justCalulated = false;
+
+const NUMERIC_KEYS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+const OPERATORS = ['%', '÷', 'x', '-', '+'];
+const DECIMAL_POINT = '.'
+const EQUALS = '='
+const ALL_CLEAR = 'AC'
+
+const NUMERIC_KEY = 'NUMERIC_KEY';
+const OPERATOR = 'OPERATOR';
 
 app.use(express.static('public'));
 
@@ -14,14 +27,175 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     socket.emit('calculationsUpdate', last10Calculations);
-    socket.on('calculation', (value) => {
-        updateCalculationCache(value);
+    socket.emit('mouseCoordinates', last10Calculations);
+    io.emit('calculatorStateChange', { equation, previousEquation, previousResult, justCalulated});
+    socket.on('calculation', (newCalculation) => {
+       
+    })
+    socket.on('mouseMove', (value) => {
+        value.socketID = socket.id;
+        socket['coordinates'] = value;
+        emitMouseCoordinates();
+    })
+    socket.on('keyPress', (value) => {
+        // comparing equation state to handle for potential latency issues
+        if(JSON.stringify(equation) === JSON.stringify(value.calculatorState.equation)) {
+            io.emit("keyPress", {key: value.key, socketID: socket.id});
+            handleKeyClick(value.key);
+        }
+    })
+    socket.on('disconnect', () => {
+        emitMouseCoordinates();
     })
 });
 
-function updateCalculationCache(newCalculation) {
-    last10Calculations = [newCalculation, ...last10Calculations.slice(0, 9)];
+function emitMouseCoordinates() {
+    const allClients = Object.values(io.sockets.sockets);
+    io.emit('mouseCoordinates', allClients.map(s => s.coordinates));
+}
+
+function handleKeyClick(key) {
+    const keyType = determineKeyType(key);
+    switch (keyType) {
+        case NUMERIC_KEY:
+            handleNumericKeyPress(key);
+            justCalulated = false;
+            break;
+        case DECIMAL_POINT:
+            handleDecimalKeyPress();
+            justCalulated = false;
+            break;
+        case OPERATOR:
+            handleOperatorKeyPress(key);
+            justCalulated = false;
+            break;
+        case ALL_CLEAR:
+            handleClearEquation();
+            justCalulated = false;
+            break;
+        case EQUALS:
+            handleEqualsKeyPress();
+            break;
+    }
+    io.emit('calculatorStateChange', { equation, previousEquation, previousResult, justCalulated});
+}
+
+function handleNumericKeyPress(key) {
+    if (!equation.length) {
+        equation.push(key)
+    } else {
+        const lastElementOfEquation = equation[equation.length - 1];
+        if (isNumber(lastElementOfEquation)) {
+            appendToExistingNumber(key);
+        } else {
+            equation.push(key)
+        }
+    }
+}
+
+function handleDecimalKeyPress() {
+    const lastElementOfEquation = equation[equation.length - 1];
+    if (!lastElementOfEquation) {
+        equation.push('0.');
+    } else {
+        if (lastElementOfEquation.includes(DECIMAL_POINT)) {
+            // cannot have two decimal points in an equation element
+            return;
+        } else if (isNumber(lastElementOfEquation)) {
+            appendToExistingNumber(DECIMAL_POINT);
+        } else {
+            // if starting a number with a decimal, include leading zero
+            equation.push('0.');
+        }
+    }
+}
+
+function appendToExistingNumber(key) {
+    const newNumber = equation[equation.length - 1].toString() + key.toString();
+
+    // only situation to display a leading 0 is '0.'
+    const newNumberClean = newNumber.startsWith('0.') ? newNumber : newNumber.replace(/^0+/, '');
+    equation[equation.length - 1] = newNumberClean;
+}
+
+function isNumber(equationElement) {
+    return equationElement.toString().split('').some(character => NUMERIC_KEYS.includes(character) || character == DECIMAL_POINT)
+}
+
+function determineKeyType(key) {
+    if (NUMERIC_KEYS.includes(key)) {
+        return NUMERIC_KEY;
+    } else if (OPERATORS.includes(key)) {
+        return OPERATOR;
+    } else {
+        // all keys which are not operators or numberic are represented as themselves as they are not part of a group of related keys
+        return key;
+    }
+}
+
+function handleEqualsKeyPress() {
+    // must be an odd number of elements to the equation and at least three elements to calculate
+    if (equation.length >= 3 && equation.length % 2 === 1) {
+        calculate();
+    }
+}
+
+function calculate() {
+    previousEquation = equation;
+
+    // replace "pretty" representations of operators with their valid js equivalent
+    const jsValidEquation = equation.map(element => {
+        if (element === '÷') {
+            return '/';
+        }
+        if (element === 'x') {
+            return '*';
+        }
+        return element;
+    })
+
+    const validJSEquationString = jsValidEquation.join("");
+    const result = eval(validJSEquationString);
+    previousResult = result;
+
+    last10Calculations = [{input: equation.join(" "), result}, ...last10Calculations.slice(0, 9)];
     io.emit('calculationsUpdate', last10Calculations);
+
+    equation = [];
+    justCalulated = true;
+}
+
+function removeTrailingDecimal(element) {
+    const splitNumber = element.toString().split('')
+    const splitRemovedDecimal = splitNumber.slice(0, splitNumber.length - 1);
+    return splitRemovedDecimal.join('');
+}
+
+function handleClearEquation() {
+    equation = [];
+    previousEquation = [];
+    previousResult = 0;
+    justCalulated = false;
+}
+
+
+function handleOperatorKeyPress(key) {
+    if (justCalulated) {
+        equation.push(previousResult)
+    }
+    if (!equation.length) {
+        return;
+    }
+    const lastElementOfEquation = equation[equation.length - 1];
+    if (OPERATORS.includes(lastElementOfEquation)) {
+        equation[equation.length - 1] = key;
+    }
+    if (isNumber(lastElementOfEquation)) {
+        if (lastElementOfEquation.toString().endsWith('.')) {
+            equation[equation.length - 1] = removeTrailingDecimal(equation[equation.length - 1]);
+        }
+        equation.push(key);
+    }
 }
 
 http.listen(port);
--- public/index.js
+++ public/index.js
@@ -2,28 +2,27 @@ document.addEventListener("DOMContentLoaded", function () {
     setUpCalculator();
     setUpSocket();
 });
-let equation = [0];
-let previousEquation = [];
-let previousResult = 0;
-let justCalulated = false;
+
 let socket;
 let last10Calculations = [];
-
-const NUMERIC_KEYS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-const OPERATORS = ['%', '÷', 'x', '-', '+'];
-const DECIMAL_POINT = '.'
-const EQUALS = '='
-const ALL_CLEAR = 'AC'
-
-const NUMERIC_KEY = 'NUMERIC_KEY';
-const OPERATOR = 'OPERATOR';
-
+let calculatorRect;
+let calculatorContainer;
+let calculatorState;
+let previousKeyPresses;
+let allKeys;
 
 function setUpCalculator() {
-    const allKeys = document.getElementsByClassName('key');
-    Array.from(allKeys).forEach(element => {
+    allKeys = Array.from(document.getElementsByClassName('key'));
+    allKeys.forEach(element => {
         element.addEventListener("click", handleKeyClick);
     });
+    calculatorContainer = document.getElementsByClassName('container')[0];
+    calculatorRect = calculatorContainer.getBoundingClientRect();
+    calculatorContainer.addEventListener('mousemove', e => {
+        const percentY = (e.clientY - calculatorRect.top) / (calculatorRect.bottom - calculatorRect.top)
+        const percentX = (e.clientX - calculatorRect.left) / (calculatorRect.right - calculatorRect.left)
+        socket.emit('mouseMove', { percentX, percentY });
+    })
 }
 
 function setUpSocket() {
@@ -32,6 +31,72 @@ function setUpSocket() {
         last10Calculations = value;
         setHistoryDisplay()
     });
+    socket.on('mouseCoordinates', (value) => {
+        drawMice(value);
+    });
+    socket.on('calculatorStateChange', (value) => {
+        calculatorState = value;
+        syncCalculator();
+    })
+    socket.on('keyPress', (value) => {
+        if (value.socketID !== socket.id) {
+            mockKeyPress(value.key)
+        }
+    })
+}
+
+function mockKeyPress(key) {
+    const nodeToPress = allKeys.find(k => k.value == key);
+    nodeToPress.focus();
+    nodeToPress.classList.add('mock-click');
+    setTimeout(() => {
+        nodeToPress.classList.remove('mock-click');
+    }, (200));
+}
+
+function syncCalculator() {
+    setDisplay();
+}
+
+function handleKeyClick() {
+    // sending up calculator state with the keypress to prevent errors from latency
+    // if calculator state has been changed by another user, the keypress will not register
+    // more robust solution would be better
+    socket.emit('keyPress', { key: event.target.value, calculatorState });
+}
+
+function drawMice(mice) {
+    mice.forEach(m => {
+        const existingMouseForSocket = Array.from(document.getElementsByClassName(m.socketID));
+        if (existingMouseForSocket.length) {
+            // modify mouse position if it already exists
+            const leftPostion = calculatorRect.left + ((calculatorRect.right - calculatorRect.left) * m.percentX) - 7;
+            existingMouseForSocket[0].style.left = `${leftPostion}px`;
+            const topPosition = calculatorRect.top + ((calculatorRect.bottom - calculatorRect.top) * m.percentY) - 7;
+            existingMouseForSocket[0].style.top = `${topPosition}px`;
+        } else {
+            // create mouse for other users
+            if (m.socketID !== socket.id) {
+                const mouse = document.createElement('img')
+                mouse.classList.add('mouse');
+                mouse.classList.add(m.socketID);
+                mouse.id = m.socketID;
+                mouse.src = "cursor.svg"
+                const leftPostion = calculatorRect.left + ((calculatorRect.right - calculatorRect.left) * m.percentX) - 7;
+                mouse.style.left = `${leftPostion}px`;
+                const topPosition = calculatorRect.top + ((calculatorRect.bottom - calculatorRect.top) * m.percentY) - 7;
+                mouse.style.top = `${topPosition}px`;
+                calculatorContainer.appendChild(mouse);
+            }
+        }
+    })
+    // delete mouse for users that are gone
+    const allMice = Array.from(document.getElementsByClassName('mouse'));
+    const existingSockets = mice.map(m => m.socketID);
+    const oldMice = allMice.filter(m => !existingSockets.includes(m.id));
+    oldMice.forEach(mouse => {
+        mouse.remove();
+    })
 }
 
 function setHistoryDisplay() {
@@ -55,151 +120,11 @@ function setHistoryDisplay() {
     })
 }
 
-function handleKeyClick() {
-    const key = event.target.value;
-    const keyType = determineKeyType(key);
-    switch (keyType) {
-        case NUMERIC_KEY:
-            handleNumericKeyPress(key);
-            justCalulated = false;
-            break;
-        case DECIMAL_POINT:
-            handleDecimalKeyPress();
-            justCalulated = false;
-            break;
-        case OPERATOR:
-            handleOperatorKeyPress(key);
-            justCalulated = false;
-            break;
-        case ALL_CLEAR:
-            handleClearEquation();
-            justCalulated = false;
-            break;
-        case EQUALS:
-            handleEqualsKeyPress();
-            break;
-    }
-    setDisplay();
-}
-
-function handleClearEquation() {
-    equation = [];
-    previousEquation = [];
-    previousResult = 0;
-    justCalulated = false;
-}
-
-function handleOperatorKeyPress(key) {
-    if (justCalulated) {
-        equation.push(previousResult)
-    }
-    if (!equation.length) {
-        return;
-    }
-    const lastElementOfEquation = equation[equation.length - 1];
-    if (OPERATORS.includes(lastElementOfEquation)) {
-        equation[equation.length - 1] = key;
-    }
-    if (isNumber(lastElementOfEquation)) {
-        if (lastElementOfEquation.toString().endsWith('.')) {
-            equation[equation.length - 1] = removeTrailingDecimal(equation[equation.length - 1]);
-        }
-        equation.push(key);
-    }
-}
-
-function removeTrailingDecimal(element) {
-    const splitNumber = element.toString().split('')
-    const splitRemovedDecimal = splitNumber.slice(0, splitNumber.length - 1);
-    return splitRemovedDecimal.join('');
-}
-
-function handleNumericKeyPress(key) {
-    if (!equation.length) {
-        equation.push(key)
-    } else {
-        const lastElementOfEquation = equation[equation.length - 1];
-        if (isNumber(lastElementOfEquation)) {
-            appendToExistingNumber(key);
-        } else {
-            equation.push(key)
-        }
-    }
-}
-
-function handleDecimalKeyPress() {
-    const lastElementOfEquation = equation[equation.length - 1];
-    if (!lastElementOfEquation) {
-        equation.push('0.');
-    } else {
-        if (lastElementOfEquation.includes(DECIMAL_POINT)) {
-            // cannot have two decimal points in an equation element
-            return;
-        } else if (isNumber(lastElementOfEquation)) {
-            appendToExistingNumber(DECIMAL_POINT);
-        } else {
-            // if starting a number with a decimal, include leading zero
-            equation.push('0.');
-        }
-    }
-}
-
-function appendToExistingNumber(key) {
-    const newNumber = equation[equation.length - 1].toString() + key.toString();
-
-    // only situation to display a leading 0 is '0.'
-    const newNumberClean = newNumber.startsWith('0.') ? newNumber : newNumber.replace(/^0+/, '');
-    equation[equation.length - 1] = newNumberClean;
-}
-
-function isNumber(equationElement) {
-    return equationElement.toString().split('').some(character => NUMERIC_KEYS.includes(character) || character == DECIMAL_POINT)
-}
-
-function determineKeyType(key) {
-    if (NUMERIC_KEYS.includes(key)) {
-        return NUMERIC_KEY;
-    } else if (OPERATORS.includes(key)) {
-        return OPERATOR;
-    } else {
-        // all keys which are not operators or numberic are represented as themselves as they are not part of a group of related keys
-        return key;
-    }
-}
-
-function handleEqualsKeyPress() {
-    // must be an odd number of elements to the equation and at least three elements to calculate
-    if (equation.length >= 3 && equation.length % 2 === 1) {
-        calculate();
-    }
-}
-
-function calculate() {
-    previousEquation = equation;
-
-    // replace "pretty" representations of operators with their valid js equivalent
-    const jsValidEquation = equation.map(element => {
-        if (element === '÷') {
-            return '/';
-        }
-        if (element === 'x') {
-            return '*';
-        }
-        return element;
-    })
-
-    const validJSEquationString = jsValidEquation.join("");
-    const result = eval(validJSEquationString);
-    previousResult = result;
-    socket.emit('calculation', { input: equation.join(" "), result: result });
-    equation = [];
-    justCalulated = true;
-}
-
 function setDisplay() {
     /* immediately after a calculation show equation in small display
     * after new equation is started show the result of the previous equation
     * inspired by google calculator */
+    const { justCalulated, previousCalculation, previousResult, previousEquation, equation } = calculatorState;
     const smallDisplayText = justCalulated ? previousEquation.join(" ") : previousResult;
     const largeDisplayText = justCalulated ? previousResult : equation.join(" ");
     document.getElementsByClassName('sub-display-small')[0].innerHTML = smallDisplayText;
